ea3f819902ed85bb15be1c10a99d537d77a7a977
juliancho1025 <juliancho1025@5d841f15-7d8e-bab8-a43a-6fffbe68d8bc>
2012-04-02 22:28:04 +0000

    firmas de clases control

---
+++ trunk/CajeroAutomatico/src/Control/PersonControl.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+/**
+ *
+ * @author julian David Peña G.
+ */
+public class PersonControl {
+    
+}
